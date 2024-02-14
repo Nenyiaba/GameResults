4a2abcebe5026b7553748a858f0ed08f1bb98417
Keex Nenyiaba <keex.nenyiaba@gm.com>
2024-02-14 09:42:34 +0000

    first commit

---
+++ src/main/java/com/example/gameResults/GameResultsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameResults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameResultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameResultsApplication.class, args);
+	}
+
+}
